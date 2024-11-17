94f3f521b950516a829aa4d2e151aba01f4d590b
naattxx <31317800+naattxx@users.noreply.github.com>
2024-11-17 14:32:18 +0000

    basic calc functionality

---
+++ calc.js
@@ -0,0 +1,58 @@
+const n = document.querySelectorAll(".n");
+const pl = document.querySelector(".pl");
+const mn = document.querySelector(".mn");
+const ml = document.querySelector(".ml");
+const dv = document.querySelector(".dv");
+const eq = document.querySelector(".eq");
+const calcScreen = document.querySelector(".calculator output");
+
+let sum = 0;
+let lastAction = (x, screenContent) => screenContent;
+let isNewInput = false;
+let isFirstType = true;
+
+function handelNumpad(input) {
+  const calcText = calcScreen.textContent;
+
+  if (isFirstType) sum = Number(input);
+  if (!isNewInput) calcScreen.textContent = input;
+  else if (calcText.length <= 10) {
+    calcScreen.textContent += input;
+  }
+
+  isNewInput = true;
+}
+
+function act(action = lastAction) {
+  if (isNewInput && !isFirstType) {
+    sum = action(sum, Number(calcScreen.textContent));
+    calcScreen.textContent = String(sum);
+  }
+  lastAction = action;
+  isNewInput = false;
+  isFirstType = false;
+}
+
+function equal() {
+  act();
+}
+
+n.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    handelNumpad(e.target.textContent);
+  });
+});
+
+pl.addEventListener("click", function () {
+  act((x, y) => x + y);
+});
+mn.addEventListener("click", function () {
+  act((x, y) => x - y);
+});
+ml.addEventListener("click", function () {
+  act((x, y) => x * y);
+});
+dv.addEventListener("click", function () {
+  act((x, y) => x / y);
+});
+eq.addEventListener("click", equal);
