d8a4963ca11b8f771bfdba17e80ca5621f33a3b9
naattxx <31317800+naattxx@users.noreply.github.com>
2024-11-22 00:32:18 +0000

    fix

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { useState } from "react";
+import { useRef } from "react";
 
 function Button({ className, onClick, children }) {
   return (
@@ -22,109 +23,112 @@ function NumButton({ onClick, children }) {
   );
 }
 
-function Screen({content}) {
+function Screen({ content }) {
   return <output>{content}</output>;
 }
 
 function Calculator() {
-  const {sum, setSum} = useState(0);
-  const {lastAction, setLastAction} = useState((x, screenContent) => screenContent);
-  const {isNewInput, setIsNewInput} = useState(false);
-  const {isFirstType, setIsFirstType} = useState(true);
-  const {screenText, setScreenText} = useState("0");
+  const sum = useRef(0);
+  const [lastAction, setLastAction] = useState(
+    () => (x, screenContent) => screenContent
+  );
+  const isFirstType = useRef(false);
+  const isFirstInput = useRef(true);
+  const [screenText, setScreenText] = useState("0");
 
   function handelNumpad(input) {
-
-    if (!isNewInput) setScreenText(input);
-    else if (screenText.length <= 10) {
-      setScreenText(screenText+input);
+    if (!isFirstType.current) setScreenText(input);
+    else if (screenText.length <= 100) {
+      setScreenText(screenText + input);
     }
-    if (isFirstType) setSum(Number(screenText));
+    if (isFirstInput.current) sum.current = Number(screenText + input);
 
-    setIsNewInput = true;
+    isFirstType.current = true;
   }
 
   function act(action = lastAction) {
-    if (isNewInput && !isFirstType) {
-      setSum(action(sum, Number(screenText)));
-      setScreenText(String(sum));
+    if (isFirstType.current && !isFirstInput.current) {
+      sum.current = action(sum.current, Number(screenText));
+      setScreenText(String(sum.current));
     }
-    setLastAction(action);
-    setIsNewInput(false);
-    setIsFirstType(false);
+    setLastAction(() => action);
+    isFirstType.current = false;
+    isFirstInput.current = false;
   }
 
+  const point = () => {
+    if (screenText.endsWith(".")) setScreenText(String(Number(screenText)));
+    else if (!screenText.includes(".")) setScreenText(screenText + ".");
+  };
+
+  const sqrt = () => {
+    sum.current = Math.sqrt(Number(screenText));
+    setScreenText(String(sum.current));
+    isFirstInput.current = false;
+  };
+
   return (
-    <>
-      <div className="calculator">
-        <Screen content={screenText} />
-        <NumButton onClick={handelNumpad}>1</NumButton>
-        <NumButton onClick={handelNumpad}>2</NumButton>
-        <NumButton onClick={handelNumpad}>3</NumButton>
-        <NumButton onClick={handelNumpad}>4</NumButton>
-        <NumButton onClick={handelNumpad}>5</NumButton>
-        <NumButton onClick={handelNumpad}>6</NumButton>
-        <NumButton onClick={handelNumpad}>7</NumButton>
-        <NumButton onClick={handelNumpad}>8</NumButton>
-        <NumButton onClick={handelNumpad}>9</NumButton>
-        <NumButton onClick={handelNumpad}>0</NumButton>
-        <Button
-          className="dv"
-          onClick={() => {
-            act((x, y) => x / y);
-          }}
-        >
-          ÷
-        </Button>
-        <Button
-          className="ml"
-          onClick={() => {
-            act((x, y) => x * y);
-          }}
-        >
-          ×
-        </Button>
-        <Button
-          className="mn"
-          onClick={() => {
-            act((x, y) => x - y);
-          }}
-        >
-          -
-        </Button>
-        <Button
-          className="pl"
-          onClick={() => {
-            act((x, y) => x + y);
-          }}
-        >
-          +
-        </Button>
-        <Button
-          className="pt"
-          onClick={function () {
-            if (screenText.endsWith("."))
-              setScreenText(String(Number(screenText)));
-            else if (!screenText.includes("."))
-              setScreenText(screenText + ".");
-          }}
-        >
-          .
-        </Button>
-        <Button
-          className="sq"
-          onClick={function () {
-            setScreenText(String(Math.sqrt(Number(screenText))));
-            isFirstType = false;
-          }}
-        >
-          √
-        </Button>
-        <Button className="eq" onClick={act}>
-          =
-        </Button>
-      </div>
-    </>
+    <div className="calculator">
+      <Screen content={String(screenText)} />
+      <NumButton onClick={handelNumpad}>1</NumButton>
+      <NumButton onClick={handelNumpad}>2</NumButton>
+      <NumButton onClick={handelNumpad}>3</NumButton>
+      <NumButton onClick={handelNumpad}>4</NumButton>
+      <NumButton onClick={handelNumpad}>5</NumButton>
+      <NumButton onClick={handelNumpad}>6</NumButton>
+      <NumButton onClick={handelNumpad}>7</NumButton>
+      <NumButton onClick={handelNumpad}>8</NumButton>
+      <NumButton onClick={handelNumpad}>9</NumButton>
+      <NumButton onClick={handelNumpad}>0</NumButton>
+      <Button
+        className="dv"
+        onClick={() => {
+          act((x, y) => x / y);
+        }}
+      >
+        ÷
+      </Button>
+      <Button
+        className="ml"
+        onClick={() => {
+          act((x, y) => x * y);
+        }}
+      >
+        ×
+      </Button>
+      <Button
+        className="mn"
+        onClick={() => {
+          act((x, y) => x - y);
+        }}
+      >
+        -
+      </Button>
+      <Button
+        className="pl"
+        onClick={() => {
+          act((x, y) => x + y);
+        }}
+      >
+        +
+      </Button>
+      <Button className="pt" onClick={point}>
+        .
+      </Button>
+      <Button className="sq" onClick={sqrt}>
+        √
+      </Button>
+      <Button
+        className="eq"
+        onClick={
+          (/*e*/) => {
+            act();
+          }
+        }
+      >
+        =
+      </Button>
+    </div>
   );
 }
 
