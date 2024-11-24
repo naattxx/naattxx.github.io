82a55ea4829c965e7d502e9cb7fcefbf6e8cde81
naattxx <31317800+naattxx@users.noreply.github.com>
2024-11-24 08:08:29 +0000

    added theme-color

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -1,25 +1,30 @@
-import * as React from "react"
-import { useSiteMetadata } from "../hooks/use-site-metadata"
+import * as React from "react";
+import { useSiteMetadata } from "../hooks/use-site-metadata";
 
 const Head = ({ title, description, children }) => {
-    const { title: defaultTitle, description: defaultDescription, author } = useSiteMetadata()
+  const {
+    title: defaultTitle,
+    description: defaultDescription,
+    author,
+  } = useSiteMetadata();
 
-    const seo = {
-        title: title || defaultTitle,
-        description: description || defaultDescription,
-        author: author,
-    }
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    author: author,
+  };
 
-    return (
-        <>
-            <meta charset="utf-8" />
-            <meta name="viewport" content="width=device-width,initial-scale=1" />
-            <title>{seo.title}</title>
-            <meta name="description" content={seo.description} />
-            <meta name="author" content={seo.author} />
-            {children}
-        </>
-    )
-}
+  return (
+    <>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <title>{seo.title}</title>
+      <meta name="description" content={seo.description} />
+      <meta name="author" content={seo.author} />
+      <meta name="theme-color" content="#bc42f5" />
+      {children}
+    </>
+  );
+};
 
-export default Head
+export default Head;
